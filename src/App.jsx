cbc8f34b4d17a0a5cb7fbf5d49789bac59fbc154
webdevSandy <samratgill8204@gmail.com>
2024-09-09 03:44:52 +0530

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
-import {} from 'react'
-import Navbar from './components/Navbar'
-import Home from './components/Home'
-import Skills from './components/Skills'
-import Projects from './components/Projects'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
+import {} from "react";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Skills from "./components/Skills";
+// import Projects from './components/Projects'
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className='font-Ubuntu bg-slate-200'>
-    <Navbar/>
-    <Home/>
-    <Skills/>
-    <Projects />
+    <div className="font-Ubuntu bg-slate-200">
+      <Navbar />
+      <Home />
+      <Skills />
+      {/* <Projects /> */}
 
-    <Contact/>
-    <Footer/>
-    
+      <Contact />
+      <Footer />
     </div>
-   
-  )
+  );
 }
 
-export default App
+export default App;
